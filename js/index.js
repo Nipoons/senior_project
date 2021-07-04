fbbd8d4c9da40794d2c8c9eeece3c06076d6c702
Nipun Vanaprasert <nipun@Nipuns-MacBook-Pro.local>
2021-07-04 09:18:53 +0700

    full webaplication

--- js/index.js
+++ js/index.js
@@ -2,23 +2,6 @@ var ros = new ROSLIB.Ros({
     url: 'ws://localhost:9090'
 });
 
-var txt_listener = new ROSLIB.Topic({
-    ros: ros,
-    name: '/txt_msg',
-    messageType: 'std_msgs/String'
-});
-
-var cmd_vel_listener = new ROSLIB.Topic({
-    ros: ros,
-    name: "/cmd_body",
-    messageType: 'geometry_msgs/Twist'
-});
-
-var txt_listener2 = new ROSLIB.Topic({
-    ros: ros,
-    name: '/txt_msg',
-    messageType: 'std_msgs/String'
-});
 var number_obs = new ROSLIB.Topic({
     ros: ros,
     name: '/num_obs',
@@ -43,9 +26,6 @@ var cmd_vel_listener2 = new ROSLIB.Topic({
     messageType: 'geometry_msgs/Twist'
 });
 
-txt_listener2.subscribe(function (k) {
-    document.getElementById("msg2").textContent = k.data;
-});
 
 number_obs.subscribe(function (k) {
     document.getElementById("num_objects").textContent = k.data;
@@ -95,11 +75,6 @@ var button = new ROSLIB.Topic({
     messageType: "std_msgs/Int16MultiArray"
 });
 
-var max = new ROSLIB.Topic({
-    ros: ros,
-    name: "/max",
-    messageType: "std_msgs/Int16MultiArray"
-});
 
 but_arr = function (data) {
     var j = new ROSLIB.Message({
@@ -175,7 +150,7 @@ window.onload = function () {
     var mw = sizemw - 10 - 23;
 
 
-    console.log(sizew)
+    
     var h = sizeh - 170;
     var w = sizew - 10 - 23;
     createViewer2(h, w);
@@ -274,8 +249,6 @@ function mode() {
             });
         });
 
-
-        // document.getElementById("onoff").textContent = "On";
         document.getElementById("togglemode").className = "btn btn-outline-primary mt-1 ms-2 mb-2";
         document.getElementById("modechange").textContent = "Manual";
         document.getElementById("joint_cont").style.display = "initial";
@@ -284,17 +257,12 @@ function mode() {
             document.getElementById("hidebutt" + i).className = "btn btn-outline-dark mt-1 ms-2 mb-2 disabled";
         }
 
-
-
-
         aj = function (data) {
             var j = new ROSLIB.Message({
                 data: data
             });
             button2.publish(j);
         }
-        // aj(a);
-
 
         var slider1 = document.getElementById("myRange");
         var output1 = document.getElementById("demo");
@@ -308,7 +276,6 @@ function mode() {
         var output5 = document.getElementById("demo5");
         var slider6 = document.getElementById("myRange6");
         var output6 = document.getElementById("demo6");
-
 
         slider1.value = a[0];
         slider2.value = a[1];
@@ -324,14 +291,9 @@ function mode() {
         output5.textContent = slider5.value;
         output6.textContent = slider6.value;
 
-
-
-
         slider1.oninput = function () {
             output1.textContent = this.value;
-
         }
-
         slider1.onchange = function () {
             var join = output1.textContent
             console.log(join)
@@ -339,18 +301,18 @@ function mode() {
             console.log(a)
             aj(a);
         }
+
         slider2.oninput = function () {
             output2.textContent = this.value;
         }
-
         slider2.onchange = function () {
             output2.textContent = this.value;
             var join2 = output2.textContent
             a[1] = Number(join2)
-
             console.log(a)
             aj(a);
         }
+
         slider3.oninput = function () {
             output3.textContent = this.value;
         }
@@ -358,10 +320,10 @@ function mode() {
             output3.textContent = this.value;
             var join3 = output3.textContent
             a[2] = Number(join3)
-
             console.log(a)
             aj(a);
         }
+
         slider4.oninput = function () {
             output4.textContent = this.value;
         }
@@ -369,10 +331,10 @@ function mode() {
             output4.textContent = this.value;
             var join4 = output4.textContent
             a[3] = Number(join4)
-
             console.log(a)
             aj(a);
         }
+
         slider5.oninput = function () {
             output5.textContent = this.value;
         }
@@ -380,10 +342,10 @@ function mode() {
             output5.textContent = this.value;
             var join5 = output5.textContent
             a[4] = Number(join5)
-
             console.log(a)
             aj(a);
         }
+
         slider6.oninput = function () {
             output6.textContent = this.value;
         }
@@ -391,7 +353,6 @@ function mode() {
             output6.textContent = this.value;
             var join6 = output6.textContent
             a[5] = Number(join6)
-
             console.log(a)
             aj(a);
         }
@@ -401,41 +362,25 @@ function mode() {
         console.log("Manual");
     }// power on
 }
-function clearReset() {
-    document.getElementById("shape").textContent = "-";
-    document.getElementById("color").textContent = "-";
-    document.getElementById("color").style.color = "black"
-    document.getElementById('cir').style.display = 'none'
-    document.getElementById('sqr').style.display = 'none'
-
-    console.log("reset");
-}//reset shape color and position
 var button2 = new ROSLIB.Topic({
     ros: ros,
     name: "/slideControl",
     messageType: "std_msgs/Float32MultiArray"
 });
-
 var joint_states = new ROSLIB.Topic({
     ros: ros,
     name: "joint_states",
     messageType: "sensor_msgs/JointState"
 });
-
 var loc = new ROSLIB.Topic({
     ros: ros,
     name: "/pos_obs",
     messageType: "std_msgs/Float32MultiArray"
 });
-
 var a = [];
-
 joint_states.subscribe(function (m) {
     var val = m.position[0];
-    // console.log(val);
-    a = m.position;
-
-    //feedback joint states
+    a = m.position;  //feedback joint states
 });
 
 var pos_obs_sub = []
@@ -445,14 +390,11 @@ loc.subscribe(function (m) {
     pos_obs_sub = m.data
 });
 
-// while (pos_obs_sub.length) newPos.push(pos_obs_sub.splice(0, 3));
-
 var loc_pub = new ROSLIB.Topic({
     ros: ros,
     name: "/to_pick_pos",
     messageType: "std_msgs/Float32MultiArray"
 });
-
 
 lp = function (data) {
     var p = new ROSLIB.Message({
@@ -461,12 +403,11 @@ lp = function (data) {
     loc_pub.publish(p);
 }
 
-var c = 1
+
 function circle() {
     document.getElementById('cir').style.display = 'initial'
     document.getElementById("cir").style.color = "black"
     document.getElementById("color").textContent = "-";
-
 
     var pos = pos_obs_sub;
     newPos = [];
@@ -496,8 +437,6 @@ function rectangle() {
     document.getElementById("color").textContent = "-";
     document.getElementById("rec").style.color = "black"
 
-
-
     var pos = pos_obs_sub;
     newPos = [];
     while (pos.length) newPos.push(pos.splice(0, 3));
@@ -512,7 +451,6 @@ function rectangle() {
     console.log("Rectangle");
     var arr = [];
     arr[1] = 4;
-    // console.log(arr);
     console.log(rt);
     but_arr(arr);
     lp(rt.flat());
@@ -656,10 +594,8 @@ function box3() {
 }
 
 function box4() {
-    // document.getElementById("shape").textContent = "Circle";
     document.getElementById('sqr').style.display = 'none'
     document.getElementById('cir').style.display = 'initial'
-
     var pos = pos_obs_sub;
     newPos = [];
     while (pos.length) newPos.push(pos.splice(0, 3));
@@ -670,19 +606,16 @@ function box4() {
             redC.push(newPos[i]);
         }
     }
-    document.getElementById("color").textContent = "Red";
+    document.getElementById("color").textContent = "Red"
     document.getElementById("color").style.color = "red"
     document.getElementById("cir").style.color = "red"
-
-
-    document.getElementById("shape").textContent = "";
+    document.getElementById("shape").textContent = ""
     console.log("Red-Circle");
     var arr = [1, 4];
     console.log(arr);
     console.log(redC);
     but_arr(arr);
     lp(redC.flat());
-    // console.log("Box3");
 }
 function emer() {
     alert("cut off");
@@ -698,7 +631,6 @@ function home() {
     pos_set_pub.publish(j)
     console.log("home");
 }
-
 function stand() {
     var j = new ROSLIB.Message({
         data: 1
@@ -707,14 +639,17 @@ function stand() {
 
     console.log("stand");
 }
+
 function login() {
-    const admin = 'admin'
-    const adminpass = 'p@ssword'
+    const admin = {
+        Id:'admin',
+        Password: 'p@ssword'
+    }
     let username = document.getElementById('username').value
     let password = document.getElementById('password').value
 
     console.log(password);
-    if (username == admin && password == adminpass) {
+    if (username == admin.Id && password == admin.Password) {
 
         console.log(username);
         location.href = 'http://127.0.0.1:5501/html/index.html'
